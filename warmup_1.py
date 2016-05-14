9c39382981171d1be08df98003fcc73c5d80a4c8
Damien Hobday <damienhobday@gmail.com>
2016-05-13 23:58:55 -0400

    7

--- warmup_1.py
+++ warmup_1.py
@@ -60,3 +60,13 @@ def makes10(a, b):
     return True
   else:
     return False
+
+
+# Near Hundred
+# Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+
+def near_hundred(n):
+  if abs(100 - n) < 11 or abs(200 - n) < 11:
+    return True
+  else:
+    return False
